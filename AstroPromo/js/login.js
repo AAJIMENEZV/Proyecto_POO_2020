f0e5a356dd83bb044019e482522f1f11527cb865
AAJIMENEZV <abnerjimenez11@gmail.com>
2020-04-02 08:10:48 -0600

    version5

--- AstroPromo/js/login.js
+++ AstroPromo/js/login.js
@@ -1,15 +1,20 @@
-$("#btn-login").click(function(){
-   let parametros = $("#form-login").serialize();
-   $.ajax({
-       url:"../ajax/cliente/?action=login",
-       method:"POST",
-       data:parametros,
-       dataType:"json",
-       success:(res)=>{
-            console.log(res);
-       },
-       error:(error)=>{
-            console.log(error);
-       }
+(function() {
+     'use strict';
+     window.addEventListener('load', function() {
+       // Fetch all the forms we want to apply custom Bootstrap validation styles to
+       var forms = document.getElementsByClassName('needs-validation');
+       // Loop over them and prevent submission
+       var validation = Array.prototype.filter.call(forms, function(form) {
+         form.addEventListener('submit', function(event) {
+           if (form.checkValidity() === false) {
+             event.preventDefault();
+             event.stopPropagation();
+           }
+           form.classList.add('was-validated');
+         }, false);
+       });
+     }, false);
+   })();
+   $(function () {
+     $('[data-toggle="tooltip"]').tooltip()
    });
-});
